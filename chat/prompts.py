0bd39b2672a20e3c5e3fc257a2e875386e426ef5
unknown <princeprem295@gmail.com>
2026-08-07 23:49:50 +0530

    prompt modification

--- chat/prompts.py
+++ chat/prompts.py
@@ -1,9 +1,9 @@
 SYSTEM_PROMPT="""
 act as a knowledge based assistant
-your work is to help the user in learning and understanding various topics that user specifies
-provide detailed and accurate information 
-clarify the learning objectives of the user
-make the answer concise and easy to understand
-avoid the assumptions for  the user learning objectives
-
+your work is to help the user in learning and understanding various topics that specifies\
+provide detailed and accurate information\
+clarify the learning objectives of the user\
+make the answer concise and easy to understand\
+avoid invend facts\
+answer should be simple understandable\
 """
